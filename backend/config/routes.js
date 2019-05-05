ca892bb45f74bcdb434f1e8c934b73bee509cec5
Matheus Miranda <mhmriranda96@gmail.com>
2019-05-05 19:11:03 -0300

    comentado e removido partes desnecessarias

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -27,40 +27,4 @@ module.exports = app => {
 
     app.route('/vendedores')
         .get(app.api.vendedor.get)
-    
-        
-//     app.route('/categories')
-//         .all(app.config.passport.authenticate())
-//         .get(admin(app.api.category.get))
-//         .post(admin(app.api.category.save))
-
-//     // Cuidado com ordem! Tem que vir antes de /categories/:id
-//     app.route('/categories/tree')
-//         .all(app.config.passport.authenticate())
-//         .get(app.api.category.getTree)
-
-//     app.route('/categories/:id')
-//         .all(app.config.passport.authenticate())
-//         .get(app.api.category.getById)
-//         .put(admin(app.api.category.save))
-//         .delete(admin(app.api.category.remove))
-
-//     app.route('/articles')
-//         .all(app.config.passport.authenticate())
-//         .get(admin(app.api.article.get))
-//         .post(admin(app.api.article.save))
-
-//     app.route('/articles/:id')
-//         .all(app.config.passport.authenticate())
-//         .get(app.api.article.getById)
-//         .put(admin(app.api.article.save))
-//         .delete(admin(app.api.article.remove))
-
-//     app.route('/categories/:id/articles')
-//         .all(app.config.passport.authenticate())
-//         .get(app.api.article.getByCategory)
-
-//     app.route('/stats')
-//         .all(app.config.passport.authenticate())
-//         .get(app.api.stat.get)
 }
