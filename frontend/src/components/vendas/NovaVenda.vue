575dc85a70951c64ad326a9373eb4a9ce2a08b32
Matheus Miranda <mhmriranda96@gmail.com>
2019-05-31 00:16:15 -0300

    testando as mascaras

--- frontend/src/components/vendas/NovaVenda.vue
+++ frontend/src/components/vendas/NovaVenda.vue
@@ -5,7 +5,7 @@
       <b-form>
         <input id="venda-id" type="hidden" v-model="venda.id">
         <b-row>
-          <b-col md="8" sm="12">
+          <b-col md="9" sm="12">
             <b-form-group label="Nome do cliente:" label-for="venda-nome">
               <!-- <b-form-input id="venda-nome" type="text" v-model="venda.nomeCliente" required/> -->
               <vue-bootstrap-typeahead
@@ -16,14 +16,14 @@
               />
             </b-form-group>
           </b-col>
-          <b-col md="4" sm="12">
+          <b-col md="3" sm="12">
             <b-form-group label="Data:" label-for="venda-data">
               <b-form-input id="venda-data" type="date" v-model="venda.data" required/>
             </b-form-group>
           </b-col>
         </b-row>
         <b-row>
-          <b-col md="3" sm="12">
+          <b-col md="2" sm="12">
             <b-form-group label="Pedido:" label-for="venda-numero-pedido">
               <b-form-input
                 id="venda-numero-pedido"
@@ -33,12 +33,22 @@
               />
             </b-form-group>
           </b-col>
-          <b-col md="3" sm="12">
+          <b-col md="2" sm="12">
             <b-form-group label="Nota Fiscal:" label-for="venda-nota-fiscal">
               <b-form-input id="venda-nota-fiscal" type="text" v-model="venda.numeroNF" required/>
             </b-form-group>
           </b-col>
-          <b-col md="3" sm="12">
+          <b-col md="2" sm="12">
+            <b-form-group label="Quantidade:" label-for="venda-quantidade">
+              <b-form-input id="venda-quantidade" type="text" v-model="venda.quantidade" required/>
+            </b-form-group>
+          </b-col>
+          <b-col md="2" sm="12">
+            <b-form-group label="Valor Total:" label-for="venda-valor-total">
+              <b-form-input id="venda-valor-total" v-model="venda.valorTotal" required/>
+            </b-form-group>
+          </b-col>
+          <b-col md="2" sm="12">
             <b-form-group label="Condição de Pagamento:" label-for="venda-condicao-pagamento">
               <b-form-select
                 id="venda-condicao-pagamento"
@@ -62,7 +72,7 @@
               </b-form-select>
             </b-form-group>
           </b-col>
-          <b-col md="3" sm="12">
+          <b-col md="2" sm="12">
             <b-form-group label="Forma de Pagamento:" label-for="venda-forma-pagamento">
               <b-form-select
                 id="venda-forma-pagamento"
@@ -82,16 +92,46 @@
         </b-row>
         <div v-for="item in pagamentosVendas" :key="item.id">
           <b-row>
-            <b-col md="3" sm="12">
+            <b-col md="2" sm="12">
               <b-form-group label="Data:" label-for="venda-data-pagamento">
-                <b-form-input id="venda-data-pagamento" type="text" v-model="item.data" required/>
+                <b-form-input id="venda-data-pagamento" type="date" v-model="item.data" required/>
               </b-form-group>
             </b-col>
-          </b-row>         
-        </div>
-         <b-row>
-            {{pagamentosVendas}}
+            <b-col md="4" sm="12">
+              <b-form-group
+                v-if="venda.formaPagamento == 'boleto'"
+                label="Número Boleto:"
+                label-for="venda-numero-boleto"
+              >
+                <b-form-input
+                  id="venda-numero-boleto"
+                  type="text"
+                  v-model="item.numeroPagamento"
+                  required
+                />
+              </b-form-group>
+              <b-form-group v-else label="Número Cheque:" label-for="venda-numero-boleto">
+                <b-form-input
+                  id="venda-numero-boleto"
+                  type="text"
+                  v-model="item.numeroPagamento"
+                  required
+                />
+              </b-form-group>
+            </b-col>
+            <b-col md="4" sm="12">
+              <b-form-group label="Valor:" label-for="venda-valor">
+                <b-form-input id="venda-valor" v-model="item.valor" required/>
+              </b-form-group>
+            </b-col>
+            <b-col md="2" sm="12">
+              <b-form-group label="Parcela:" label-for="venda-numero-parcela">
+                <b-form-input id="venda-numero-parcela" v-model="item.numeroParcela" disabled/>
+              </b-form-group>
+            </b-col>
           </b-row>
+        </div>
+        <b-row>{{pagamentosVendas}}</b-row>
         <hr>
         <b-row></b-row>
       </b-form>
@@ -126,7 +166,15 @@ export default {
       venda: {
         condicaoPagamento: 1
       },
-      vendas: [],
+      money: {
+        decimal: ",",
+        thousands: ".",
+        prefix: "R$ ",
+        suffix: "",
+        precision: 2,
+        masked: false /* doesn't work with directive */
+      },
+      // vendas: [],
       pagamentosVendas: [{}],
       clientes: [],
       clienteBusca: "",
@@ -171,22 +219,20 @@ export default {
       // const sugestoes = await res.json()
       // this.clientes = sugestoes
     },
-    preencherPagamentos(){
+    preencherPagamentos() {
+      this.venda.valorTotal = this.venda.valorTotal.replace(",", ".");
       // alert(this.venda.condicaoPagamento)
-      this.pagamentosVendas = []
-      for(let i = 0; i < parseInt(this.venda.condicaoPagamento); i++){
+      this.pagamentosVendas = [];
+      for (let i = 0; i < parseInt(this.venda.condicaoPagamento); i++) {
         // alert("aqui")
-        this.pagamentosVendas.push({})
+        this.pagamentosVendas.push({
+          valor: (
+            this.venda.valorTotal / parseInt(this.venda.condicaoPagamento)
+          ).toFixed(2),
+          numeroParcela: `${i + 1}/${this.venda.condicaoPagamento}`
+        });
       }
     }
-  },
-  mounted() {
-    //   var vm = this
-    // EventBus.$on("editarCliente", function(client2) {
-    //     alert(client2.nomeCliente + " " + client2.email)
-    //   vm.venda = { ...venda }
-    //   alert('aqui sim')
-    // });
   },
   watch: {
     clienteBusca: _.debounce(function(nome) {
