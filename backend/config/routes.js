216c91dc03e0bfe19121a08dc11ce747d2437799
Matheus Miranda <mhmriranda96@gmail.com>
2019-06-07 00:36:06 -0300

    comentando para teste

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -6,13 +6,13 @@ module.exports = app => {
     app.post('/validateToken', app.api.auth.validateToken)
 
     app.route('/users')
-        .all(app.config.passport.authenticate())
+        // .all(app.config.passport.authenticate())
         .post(app.api.user.save)
         .get(app.api.user.get)
 
     app.route('/users/:id')
-        .all(app.config.passport.authenticate())
-        .put(admin(app.api.user.save))
+        // .all(app.config.passport.authenticate())
+        .put(app.api.user.save)
         .get(app.api.user.getById)
         .delete(app.api.user.remove)
 
