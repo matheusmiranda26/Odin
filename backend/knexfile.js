7de27edbafad0ca5d4ea6c9854eec949967fc008
Matheus Miranda <mhmriranda96@gmail.com>
2019-06-07 00:36:23 -0300

    alteração para funcionar a configuração de data

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,8 +1,24 @@
-const { db } = require('./.env')
+const {
+	db
+} = require('./.env')
+var moment = require('moment');
 
 module.exports = {
 	client: 'mysql',
-	connection: db,
+	// connection: db,
+	connection: {
+		host: '127.0.0.1',
+		port: 3306,
+		database: 'odin',
+		user: 'root',
+		password: 'root',
+		typeCast: function (field, next) {
+			if (field.type == 'DATE') {
+				return moment(field.string()).format('YYYY-MM-DD');
+			}
+			return next();
+		}
+	},
 	pool: {
 		min: 2,
 		max: 10
