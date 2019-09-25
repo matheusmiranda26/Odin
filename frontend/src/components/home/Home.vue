9b1569f68052853fad729cbaf0d8547d8df16b6a
Matheus Miranda <mhmiranda96@gmail.com>
2019-09-25 17:33:25 -0300

    colocado gráfico na home

--- frontend/src/components/home/Home.vue
+++ frontend/src/components/home/Home.vue
@@ -16,14 +16,14 @@
               />
             </b-form-group>
           </b-col>
-          <b-col md="1" sm="12">
+          <!--<b-col md="1" sm="12">
             <b-input-group-append>
               <b-input-group-text>
                 <v-icon name="search"></v-icon>
               </b-input-group-text>
             </b-input-group-append>
-          </b-col>
-          <b-col md="3" sm="12">
+          </b-col>-->
+          <b-col md="4" sm="12">
             <b-form-group label-for="tipo-busca">
               <b-form-select id="tipo-busca" options v-model="tipoBusca">
                 <option value="clientes">Cliente</option>
@@ -37,14 +37,15 @@
     </div>
     <div class="stats">
       <Stat title="Clientes" icon="fa fa-user" color="#d54d50" tabela="Clientes" />
-      <Stat title="Vendas" icon="fas fa-receipt" color="#3bc480" tabela="Vendas" />
+      <Stat title="Vendas mês atual" icon="fa fa-receipt" color="#3bc480" tabela="VendaMes" />
       <Stat title="Despesas" icon="fa fa-file" color="#3bc480" tabela="Despesas" />
       <!-- <Stat title="Usuários" :value="stat.users"
-      icon="fa fa-user" color="#3282cd"-->
+      icon="fa fa-user" color="#3282cd"
+      -->
       <b-container>
         <div class="col-12">
           <div class="box">
-            <canvas id="vendas"></canvas>
+            <canvas id="grafico-vendas" width="100%" height="20%"></canvas>
           </div>
         </div>
       </b-container>
@@ -59,9 +60,11 @@ import VueBootstrapTypeahead from "vue-bootstrap-typeahead";
 import axios from "axios";
 import { baseApiUrl } from "@/global";
 import _ from "underscore";
-//import Chart from "./Chart.vue";
+//import LineChart from "./Chart.vue";
 import Chart from "chart.js";
 import Chartist from "chartist";
+import VueApexCharts from "vue-apexcharts";
+import moment from "moment";
 
 export default {
   name: "Home",
@@ -69,7 +72,8 @@ export default {
     PageTitle,
     Stat,
     "vue-bootstrap-typeahead": VueBootstrapTypeahead,
-    Chart
+    apexchart: VueApexCharts
+    //  LineChart
   },
   data: function() {
     return {
@@ -78,18 +82,14 @@ export default {
       dadoBusca: "",
       dadoSelecionado: null,
       stat: {},
-      loaded: true,
-      vendas: null,
-      options: {
-        responsive: true,
-        maintainAspectRatio: false
-      }
+      loaded: false,
+      vendas: [],
+      dataVendas: [],
+      quantidadeVendas: []
     };
   },
   watch: {
     dadoBusca: _.debounce(function(nome) {
-      //  var busca = this.tipoBusca.substring(0,(this.tipoBusca.toLowerCase() -1))
-      //  this.$router.push(`/busca/${this.dadoSelecionado.id}`);
       this.get(nome);
     }, 500),
     dadoSelecionado: _.debounce(function(nome) {
@@ -100,82 +100,58 @@ export default {
         busca = this.tipoBusca.substring(0, this.tipoBusca.length - 2);
       }
       this.$router.push(`/${busca}/${this.dadoSelecionado.id}`);
-    }, 200)
+    }, 200),
+    quantidadeVendas: _.debounce(function(qtd) {
+      var ctx = document.getElementById("grafico-vendas");
+      this.chart = new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels: this.dataVendas,
+          datasets: [
+            {
+              label: "Vendas",
+              backgroundColor: "rgba(54, 162, 235, 0.5)",
+              borderColor: "rgb(54, 162, 235)",
+              fill: false,
+              data: this.quantidadeVendas
+            }
+          ]
+        },
+        options: {
+          title: {
+            display: true,
+            text: "Quantidade de Vendas Mês Atual",
+            fontSize: 15
+          },
+          legend: {
+            display: false
+          }
+        }
+      });
+    })
   },
   methods: {
     async get(nome) {
       const url = `${baseApiUrl}/${this.tipoBusca}/nome/${nome}`;
       await axios.get(url).then(res => (this.dado = res.data));
-    },
-    getVendas(){
-       this.loaded = false;
+    }
+  },
+  mounted() {
+    this.loaded = false;
     try {
-      axios
-        .get(`${baseApiUrl}/vendas`)
-        .then(res => (this.vendas = res.data));
+      axios.get(`${baseApiUrl}/vendaPorDia`).then(res => {
+        this.dataVendas = res.data.map(venda => {
+          return moment(venda.data).format("DD/MM/YYYY");
+        });
+        this.quantidadeVendas = res.data.map(venda => {
+          return venda.quantidade;
+        });
+        this.vendas = res.data;
+      });
       this.loaded = true;
     } catch (error) {
       console.log(error);
     }
-    
-    }
-  },
-  mounted() {
-    this.$nextTick(() => {
-      var ctx = document.getElementById("vendas").getContext("2d");
-      var config = {
-        type: "line",
-        data: {
-          labels: [
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-            "August",
-            "September",
-            "October",
-            "November",
-            "December"
-          ],
-          datasets: [
-            {
-              label: "CoPilot",
-              fill: false,
-              borderColor: "#284184",
-              pointBackgroundColor: "#284184",
-              backgroundColor: "rgba(0, 0, 0, 0)",
-              data: this.getVendas()
-            },
-            {
-              label: "Personal Site",
-              borderColor: "#4BC0C0",
-              pointBackgroundColor: "#4BC0C0",
-              backgroundColor: "rgba(0, 0, 0, 0)",
-              data: this.personalNumbers
-            }
-          ]
-        },
-        options: {
-          responsive: true,
-          maintainAspectRatio: !this.isMobile,
-          legend: {
-            position: "bottom",
-            display: true
-          },
-          tooltips: {
-            mode: "label",
-            xPadding: 10,
-            yPadding: 10,
-            bodySpacing: 10
-          }
-        }
-      };
-
-      new Chart(ctx, config);
-    });
   }
 };
 </script>
